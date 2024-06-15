3f97b2e2c09945005b5e6ba542fe1e3dbc68e29e
Alejo Gaston Brites <alejobrites456@gmail.com>
2024-06-14 22:46:39 -0300

    feat:  agregamos service de lote y articulo

--- src/app/core/services/articulo.service.ts
+++ src/app/core/services/articulo.service.ts
@@ -18,4 +18,15 @@ export class ArticuloService {
   getArticulos(): Observable<Articulo[]> {
     return this.http.get<Articulo[]>(this.url + this.prefix + this.articulos);
   }
+
+  crearArticulo(descripcion : string, costo: number, precioVenta:number): Observable<Articulo>{
+    let articuloDto={
+      descripcion: descripcion, costo:costo, precioVenta:precioVenta
+    }
+    return this.http.post<Articulo>(this.url + this.prefix + this.articulos,articuloDto);
+  }
+
+  eliminarArticulo(id:number): Observable<Articulo>{
+    return this.http.delete<Articulo>(this.url + this.prefix + this.articulos + "/"+ id);
+  }
 }
--- src/app/core/services/lote.service.ts
+++ src/app/core/services/lote.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import Articulo from '../models/articulo';
+import LoteArticulo from '../models/loteArticulo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoteService {
 
-  constructor() { }
+  url: string = environment.url;
+  prefix: string = environment.prefix;
+  lotes: string = environment.lotes;
+
+  constructor(private http: HttpClient) { }
+
+  getLotes(): Observable<LoteArticulo[]> {
+    return this.http.get<LoteArticulo[]>(this.url + this.prefix + this.lotes);
+  }
+
+  crearLote(idArticulo: number, cantidad:number, precioCompra:number , proveedor: string, costo:number): Observable<LoteArticulo>{
+    let loteDto={
+      idArticulo: idArticulo, cantidad:cantidad, precioCompra:precioCompra, proveedor:proveedor, costo:costo
+    }
+    return this.http.post<LoteArticulo>(this.url + this.prefix + this.lotes,loteDto);
+  }
+
+  eliminarLote(id:number): Observable<LoteArticulo>{
+    return this.http.delete<LoteArticulo>(this.url + this.prefix + this.lotes + "/"+ id);
+  }
 }
