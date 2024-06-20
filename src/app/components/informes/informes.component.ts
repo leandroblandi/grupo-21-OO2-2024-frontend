ef4cee7c89859fe60d0408fb160a775f17378b6a
Leandro Blandi <leandronahuelblandiomg@gmail.com>
2024-06-19 21:12:35 -0300

    fix: Total de ventas no se muestra

--- src/app/components/informes/informes.component.ts
+++ src/app/components/informes/informes.component.ts
@@ -68,7 +68,7 @@ export class InformesComponent implements OnInit {
   }
 
   getTotalVentas(): void{
-    this.informeService.getTotalClientes().subscribe({
+    this.informeService.getTotalVentas().subscribe({
       next: (res) => {
         this.totalVentas = res;
 
